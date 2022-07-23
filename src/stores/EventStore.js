34c32292b89d5885e250365de97f58deab46e2b4
hsimao <e087754958@gmail.com>
2022-07-23 22:54:15 +0800

    feat: eventDetail change to pinia and setup script

--- src/stores/EventStore.js
+++ src/stores/EventStore.js
@@ -18,6 +18,21 @@ export const useEventStore = defineStore("CountStore", {
         .catch((error) => {
           throw error;
         });
+    },
+    fetchEvent(id) {
+      const existingEvent = this.events.find((event) => event.id === id);
+      if (existingEvent) {
+        this.event = existingEvent;
+      } else {
+        return EventService.getEvent(id)
+          .then((response) => {
+            console.log(response);
+            this.event = response.data;
+          })
+          .catch((error) => {
+            throw error;
+          });
+      }
     }
   }
 });
